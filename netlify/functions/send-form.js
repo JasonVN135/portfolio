79e5baa53143e09699c0efd016621e6c38869709
Jason Nguyen <jvnguye6@ncsu.edu>
2025-12-09 12:52:21 -0500

    Added print debugss
    Signed-off-by: Jason Nguyen <jvnguye6@ncsu.edu>

--- netlify/functions/send-form.js
+++ netlify/functions/send-form.js
@@ -9,13 +9,14 @@ export async function handler(event, context) {
     }
 
     const { name, email, message } = JSON.parse(event.body);
-
+    
     const formData = new URLSearchParams();
     formData.append("name", name);
     formData.append("email", email);
     formData.append("message", message);
     formData.append("access_key", process.env.PORTFOLIO_FORM_ACCESS_KEY); // Netlify env variable
-
+    console.log("Form");
+    console.log(formData)
     try {
         const response = await fetch("https://api.web3forms.com/submit", {
             method: "POST",
@@ -23,20 +24,27 @@ export async function handler(event, context) {
         });
 
         const data = await response.json();
+        console.log("DATA");
+        console.log(data);
 
         if (response.ok) {
+            console.log("Response is ok");
             return {
+
                 statusCode: 200,
                 body: JSON.stringify({ success: true })
             };
         } else {
+            console.log("Response is not ok 400");
             return {
                 statusCode: 400,
                 body: JSON.stringify({ success: false, message: data.message })
             };
         }
     } catch (error) {
+        console.log("Response is not ok 560");
         return {
+            
             statusCode: 500,
             body: JSON.stringify({ success: false, message: "Server error" })
         };
